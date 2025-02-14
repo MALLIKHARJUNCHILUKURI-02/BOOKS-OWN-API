4c8f36f7ef6354abc371f65ce138b38d9f716638
MALLIKHARJUNCHILUKURI-02 <mkreddychilukuri075@gmail.com>
2025-02-14 12:16:50 +0530

    updated search functionality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import fs from "fs";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
@@ -8,7 +9,7 @@ const DATA_FILE = "books.json";
 
 app.use(cors());
 app.use(express.json());
-
+app.use(bodyParser.urlencoded({extended: true}));
 // Read books data
 const readBooks = () => {
   const data = fs.readFileSync(DATA_FILE);
@@ -27,7 +28,7 @@ app.get("/books/:id", (req, res) => {
   book ? res.json(book) : res.status(404).json({ message: "Book not found" });
 });
 
-app.get("/filter", (req,res) =>{
+app.get("/filter/author", (req,res) =>{
     const books = readBooks();
     const author = req.query.author;
 
@@ -42,6 +43,23 @@ app.get("/filter", (req,res) =>{
     } else{
         res.status(404).json({ message: "author not found" });
     }
+});
+
+app.get("/filter/genre", (req,res) =>{
+  const books = readBooks();
+  const genre = req.query.genre;
+  console.log(`genre ${genre}`);
+  if(!genre){
+      return res.status(400).json({ message: "genre query parameter is required" });
+  }
+
+  const filteredgenre = books.filter((b) => b.genre.toLowerCase() === genre.toLowerCase());
+ 
+  if(filteredgenre.length > 0){
+      res.json(filteredgenre);
+  } else{
+      res.status(404).json({ message: "genre not found" });
+  }
 });
 
 // POST a new book
@@ -66,13 +84,35 @@ app.put("/books/:id", (req, res) => {
 
 // PATCH (Partially update a book)
 app.patch("/books/:id", (req, res) => {
-  let books = readBooks();
-  const index = books.findIndex((b) => b.id === parseInt(req.params.id));
-  if (index === -1) return res.status(404).json({ message: "Book not found" });
+  // let books = readBooks();
+  // const index = books.findIndex((b) => b.id === parseInt(req.params.id));
+  // if (index === -1) return res.status(404).json({ message: "Book not found" });
 
-  books[index] = { ...books[index], ...req.body };
+  // books[index] = { ...books[index], ...req.body };
+  // fs.writeFileSync(DATA_FILE, JSON.stringify(books, null, 2));
+  // res.json(books[index]);
+
+
+  let books = readBooks();
+  const id = parseInt(req.params.id);
+  const existingBook = books.find((book) => book.id === id);
+  if (!existingBook) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  const updatedBook = {
+    id: id,
+    title: req.body.title || existingBook.title,
+    author: existingBook.author,
+    genre: existingBook.genre,
+    summary: req.body.summary || existingBook.summary,
+    publication_year: existingBook.publication_year,
+  };
+
+  const searchIndex = books.findIndex((book) => book.id === id);
+  books[searchIndex] = updatedBook;
+  console.log(books[searchIndex]);
   fs.writeFileSync(DATA_FILE, JSON.stringify(books, null, 2));
-  res.json(books[index]);
+  res.json(updatedBook);
 });
 
 // DELETE a book
